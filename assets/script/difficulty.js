5205f36c0c2aac49e3ff06a75c5193eb758df55c
Simone Goti <gz7pytfpdb@privaterelay.appleid.com>
2025-12-19 13:30:12 +0100

    termine esercizio

--- assets/script/difficulty.js
+++ assets/script/difficulty.js
@@ -19,14 +19,27 @@ else {
 const btnEasy = document.querySelector(".easy-button-color");
 const btnHard = document.querySelector(".hard-button-color");
 const confirmBtn = document.querySelector("footer a");
+const mess = document.querySelector("#messaggio");
+
+const showAllert = () => {
+  confirmBtn.addEventListener("mouseover", function () {
+    if (!confirmBtn.getAttribute("href")) {
+      mess.style.visibility = "visible";
+    }
+  });
+};
 
 btnEasy.addEventListener("click", function () {
   confirmBtn.href = "../benchmark.html";
   btnEasy.style.background = " linear-gradient(180deg, #00ffff 0%, #039b9b 80%)";
   btnHard.style.background = "";
+  mess.style.visibility = "hidden";
 });
 btnHard.addEventListener("click", function () {
   confirmBtn.href = "../benchmarkHard.html";
   btnHard.style.background = "linear-gradient(180deg, #ca0191 0%, #8a007a 80%)";
   btnEasy.style.background = "";
+  mess.style.visibility = "hidden";
 });
+
+showAllert();
